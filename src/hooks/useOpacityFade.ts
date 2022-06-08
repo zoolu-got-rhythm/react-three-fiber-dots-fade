ffd0de2d5790a02a4ec44fdb349a638904b03e69
christo morris <christo@christos-MacBook-Pro.local>
2022-06-08 04:48:53 +0100

    updated jsdoc for R3F fade hook, changed all functions within this hook to be es6 arrow function syntax for consistency

--- src/hooks/useOpacityFade.ts
+++ src/hooks/useOpacityFade.ts
@@ -3,28 +3,18 @@ import { useEffect, useRef, useState } from "react";
 import { Clock } from "three";
 
 /**
+ * IMPORTANT NOTE: when using this hook with R3F mesh components, transparency prop must = true on material of referenced mesh
  * @param {THREE.Mesh} meshRef - reference of mesh to fade
  * @param {number} durationInSeconds - fade duration in seconds 
+ * @param {function} onOpacityFadeFinished - callback that is called when opacity on material of mesh has reached 0 (from 1)
  * @returns startFade function, stopFade function and update function as array
- */
+ */ 
 export function useOpacityFade(meshRef: THREE.Mesh, durationInSeconds: number, onOpacityFadeFinished?: () => void): 
     [() => void, () => void, (clock: Clock) => void]{
-
-    // make opacity changeable by setting transparent=true
-    useEffect(()=>{
-        if(meshRef != null){
-            // // @ts-ignore
-            // meshRef.material.transparent = true;
-            // console.log("USE EFFECT IN OPACITY FACE HOOK CALLED");
-            // // @ts-ignore
-            // console.log(meshRef.material.transparent);
-        }
-    }, [meshRef]);
 
     const [fade, setFade] = useState<boolean>(false);
     const timeOfFadeStart = useRef<null | number>(null);
     const onOpacityFadeCalled = useRef<boolean>(false);
-
 
     const startFade = () => {
         onOpacityFadeCalled.current = false;
@@ -40,7 +30,7 @@ export function useOpacityFade(meshRef: THREE.Mesh, durationInSeconds: number, o
         }
     }
 
-    function update(clock: Clock){
+    const update = (clock: Clock) => {
         if(meshRef == null)
             return;
         // @ts-ignore
@@ -58,8 +48,6 @@ export function useOpacityFade(meshRef: THREE.Mesh, durationInSeconds: number, o
                 
             // @ts-ignore
             meshRef.material.opacity = Math.max(1 - ((clock.getElapsedTime() - timeOfFadeStart.current) / durationInSeconds), 0); 
-            // @ts-ignore
-            // console.log(meshRef.material.opacity);
         }
     }
 
